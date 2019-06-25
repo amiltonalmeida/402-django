afe8017b4833370fa07cc4503bc6cc02a410d864
Amilton <amiltonfelipedc@gmail.com>
2019-06-25 17:07:27 -0300

    Trabalhar com Django

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mostrar_index(request):
+    return render(request, 'index.html')
+
+def mostrar_sobre(request):
+    return render(request, 'sobre.html')
+
+def mostrar_contato(request):
+    return render(request, 'contato.html')
